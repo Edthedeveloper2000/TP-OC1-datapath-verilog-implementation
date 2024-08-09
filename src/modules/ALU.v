8353280a6d537dc2ad11d07179d54bad2bd0ef6b
Edthedeveloper200 <edmarcosmendes2019@gmail.com>
2024-08-08 22:08:19 -0300

    feat: atualiza alu

--- src/modules/ALU.v
+++ src/modules/ALU.v
@@ -1,22 +1,25 @@
+
 module ALU (
     input wire [31:0] a,
     input wire [31:0] b,
     input wire [2:0] alu_control,
     output reg [31:0] result,
-    output reg zero
+    output wire zero
 );
 
     always @(*) begin
         case (alu_control)
-            3'b000: result = a + b;
-            3'b001: result = a | b;
-            3'b010: result = a & b;
-            3'b011: result = a << b[4:0];
-            3'b100: result = a - b;
-            default: result = 32'b0;
+            3'b000: result = a + b;              // Adição
+            3'b001: result = a | b;              // OR
+            3'b010: result = a & b;              // AND
+            3'b011: result = a << b[4:0];        // Shift left lógico
+            3'b100: result = a - b;              // Subtração
+            default: result = 32'b0;             // Valor padrão para operações indefinidas
         endcase
-
-        zero = (result == 32'b0) ? 1'b1 : 1'b0;
     end
 
+    // Zero flag para indicar resultado zero
+    assign zero = (result == 32'b0);
+
 endmodule
+

22cdb6ef43c9257bfdea512f121e28843e94e121
Edthedeveloper200 <edmarcosmendes2019@gmail.com>
2024-08-08 22:04:39 -0300

    feat: atualiza input asm com instruções do grupo

--- src/input1.asm
+++ src/input1.asm
@@ -1,23 +1,19 @@
-# Colocando valor base na memória
-# addi x2, x0, 7
-# sw x2, 4(x0)
+lh x1, 4(x0)        # x1 = Mem[4] (carregando o valor da posição de memória 4 em x1)
+add x2, x1, x0      # x2 = x1 + x0 (x2 = x1, pois x0 = 0)
 
-lw x1, 4(x0)
-add x2, x1, x0
+add x1, x1, x2      # x1 = x1 + x2
+sll x1, x1, 0       # x1 = x1 << 0 (não altera o valor de x1)
+sll x1, x1, 0       # x1 = x1 << 0 (não altera o valor de x1)
+sub x1, x1, x2      # x1 = x1 - x2
+sub x1, x1, x2      # x1 = x1 - x2
 
-add x1, x1, x2
-add x1, x1, x2
-sub x1, x1, x2
-sub x1, x1, x2
-
-beq x1, x2, SAIDA
+bne x1, x2, SAIDA   # Se x1 != x2, vai para SAIDA
 
 # Caso o fluxo venha para cá, seu processador está errado
-add x1, x1, x1
-sw x1, 0(x0)
+add x1, x1, x1      # x1 = x1 + x1
+sh x1, 0(x0)        # Mem[0] = x1 (armazenando x1 na posição de memória 0)
 
 SAIDA:
-and x1, x1, x2
-or x1, x1, x0
-sw x1, 0(x0)
-
+andi x1, x1, x2     # x1 = x1 & x2
+or x1, x1, x0       # x1 = x1 | x0
+sh x1, 0(x0)        # Mem[0] = x1 (armazenando x1 na posição de memória 0)
